50b500ed374eceeafef929e03ebb916d49834eb0
MD ZAID ANWAR <126465961+Brainstorm2605@users.noreply.github.com>
2024-04-22 21:20:18 +0530

    Update rag.py

--- FAQ's RAG/rag.py
+++ FAQ's RAG/rag.py
@@ -1,5 +1,4 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import Ollama
